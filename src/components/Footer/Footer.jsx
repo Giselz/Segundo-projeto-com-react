62efe066b53e037de1f85764137b3f587d064fe7
Gisele Santos <giseledosantos@outlook.com>
2021-11-26 18:43:12 -0300

    Resolvendo o erro dos links do footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,9 @@ import './footer.style.css'
 function Footer (){
     return(    
         <div className="footer">
-            <p className="textoFooter">Feito com 💜 por <a className="linkGitHubAutora" href="https://github.com/Giselz" target="_blank">
+            <p className="textoFooter">Feito com 💜 por <a className="linkGitHubAutora" href="https://github.com/Giselz" rel="noreferrer" target="_blank">
             <span className="autora">Gisele dos Santos </span></a> 
-            | <a className="linkGitHubReprograma" href="https://reprograma.com.br/" target="_blank" > <span className="reprograma">Reprograma</span></a>
+            | <a className="linkGitHubReprograma" href="https://reprograma.com.br/" rel="noreferrer" target="_blank" > <span className="reprograma">Reprograma</span></a>
             </p>
         </div>
     )
